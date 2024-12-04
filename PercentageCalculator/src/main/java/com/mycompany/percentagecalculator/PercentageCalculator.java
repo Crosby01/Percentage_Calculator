4a3740f9c88fc8cc63adc20fe6a13d8e671eaed0
Victor <147107766+Crosby01@users.noreply.github.com>
2024-12-04 11:16:54 +0100

    Add files via upload

---
+++ PercentageCalculator/src/main/java/com/mycompany/percentagecalculator/PercentageCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.percentagecalculator;
+
+/**
+ *
+ * @author Victor
+ */
+public class PercentageCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
